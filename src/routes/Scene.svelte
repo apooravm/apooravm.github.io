5870c8b97af057cf230318cf19010c4e0e2ab9f0
apooravm <medal.apoorav@gmail.com>
2024-01-30 20:15:14 +0530

    revolution and rotation + cleaner code

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,15 +1,35 @@
-<script>
+<script lang="ts">
 	import { T, useTask } from '@threlte/core';
 	import { interactivity, OrbitControls } from '@threlte/extras';
 	import { spring } from 'svelte/motion';
-	import { AxesHelper } from 'three';
+	import type { DoubleSide } from 'three';
+    import { DirectionalLightHelper } from 'three';
+
+	type Position = {
+		x: number;
+		y: number;
+		z: number;
+	};
 
 	interactivity();
 	const scale = spring(1);
 
 	let rotation = 0;
+	let mainSpherePos: Position = { x: 0, y: 0, z: 0 };
+	let subSpherePos: Position = { x: 0, y: 0, z: 0 };
+
+	const mainRevolutionRad = 8;
+	const subRevolutionRad = 5;
+	const speedA = 1;
+	const speedB = 2;
+
 	useTask((delta) => {
 		rotation += delta;
+		mainSpherePos.y = Math.sin(rotation * speedA) * mainRevolutionRad;
+		mainSpherePos.z = Math.cos(rotation * speedA) * mainRevolutionRad;
+
+		subSpherePos.y = mainSpherePos.y + Math.sin(rotation * speedB) * subRevolutionRad;
+		subSpherePos.z = mainSpherePos.z + Math.cos(rotation * speedB) * subRevolutionRad;
 	});
 </script>
 
@@ -25,7 +45,7 @@
 
 <T.DirectionalLight position={[0, 10, 10]} castShadow />
 
-<T.AxesHelper args={[2]}/>
+<T.AxesHelper args={[2]} />
 
 <T.Mesh
 	position.y={1}
@@ -36,18 +56,17 @@
 	castShadow
 >
 	<T.BoxGeometry args={[1, 1, 1]} />
-	<T.MeshStandardMaterial color="hotpink" wireframe={true}/>
+	<T.MeshStandardMaterial color="hotpink" wireframe={true} />
 </T.Mesh>
 
-<T.PointLight position.y={1} intensity={5}/>
+<T.PointLight position.y={1} intensity={5} castShadow />
 
-<T.Mesh position.y={Math.sin(rotation)*5} position.x={Math.cos(rotation)*5} receiveShadow>
-	<T.SphereGeometry args={[2, 25, 25]}/>
-	<T.MeshStandardMaterial color="white" side={2}/>
+<T.Mesh position.y={mainSpherePos.y} position.z={mainSpherePos.z} receiveShadow castShadow>
+	<T.SphereGeometry args={[2, 25, 25]} />
+	<T.MeshStandardMaterial color="white" />
 </T.Mesh>
 
-
-<T.Mesh position.y={Math.sin(rotation)*5 + Math.sin(rotation*1.5)*4} position.x={Math.cos(rotation)*5 + Math.cos(rotation*1.5)*4} receiveShadow>
-	<T.SphereGeometry args={[1, 25, 25]}/>
-	<T.MeshStandardMaterial color="white" side={2}/>
+<T.Mesh position.y={subSpherePos.y} position.z={subSpherePos.z} receiveShadow castShadow>
+	<T.SphereGeometry args={[1, 25, 25]} />
+	<T.MeshStandardMaterial color="white" />
 </T.Mesh>
