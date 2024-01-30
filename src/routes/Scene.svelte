ea75f9f7f353dcbd4d090a4b6ca7cbe15d5c7f9f
apooravm <medal.apoorav@gmail.com>
2024-01-30 18:48:29 +0530

    revolution and rotation

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,14 +1,14 @@
 <script>
 	import { T, useTask } from '@threlte/core';
-	import { interactivity } from '@threlte/extras';
+	import { interactivity, OrbitControls } from '@threlte/extras';
 	import { spring } from 'svelte/motion';
+	import { AxesHelper } from 'three';
 
 	interactivity();
 	const scale = spring(1);
 
 	let rotation = 0;
 	useTask((delta) => {
-		console.log(delta);
 		rotation += delta;
 	});
 </script>
@@ -19,9 +19,13 @@
 	on:create={({ ref }) => {
 		ref.lookAt(0, 0, 0);
 	}}
-/>
+>
+	<OrbitControls />
+</T.PerspectiveCamera>
 
 <T.DirectionalLight position={[0, 10, 10]} castShadow />
+
+<T.AxesHelper args={[2]}/>
 
 <T.Mesh
 	position.y={1}
@@ -32,10 +36,18 @@
 	castShadow
 >
 	<T.BoxGeometry args={[1, 1, 1]} />
-	<T.MeshStandardMaterial color="hotpink" wireframe={true} />
+	<T.MeshStandardMaterial color="hotpink" wireframe={true}/>
 </T.Mesh>
 
-<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
-	<T.CircleGeometry args={[4, 40]} />
-	<T.MeshStandardMaterial color="white" />
+<T.PointLight position.y={1} intensity={5}/>
+
+<T.Mesh position.y={Math.sin(rotation)*5} position.x={Math.cos(rotation)*5} receiveShadow>
+	<T.SphereGeometry args={[2, 25, 25]}/>
+	<T.MeshStandardMaterial color="white" side={2}/>
+</T.Mesh>
+
+
+<T.Mesh position.y={Math.sin(rotation)*5 + Math.sin(rotation*1.5)*4} position.x={Math.cos(rotation)*5 + Math.cos(rotation*1.5)*4} receiveShadow>
+	<T.SphereGeometry args={[1, 25, 25]}/>
+	<T.MeshStandardMaterial color="white" side={2}/>
 </T.Mesh>
