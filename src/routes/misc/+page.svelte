1661e65815d77ca5246ccd200932badb20c5ac76
apooravm <medal.apoorav@gmail.com>
2024-02-02 11:10:41 +0530

    did something, forgor

--- src/routes/misc/+page.svelte
+++ src/routes/misc/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     const notes = [
+        "tbh i dunno how i fixed it or what the prob even was. I thought the prob was in relation to the undervolting/overclocking business i had done a few years back following that yt channel. I tried to reverse what he had done. Installed afterburner and changed some settings didnt touch th eoverclock thing. Then dunno. It restarted and was fixed?",
         "today was nice",
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to",
         "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33",
@@ -22,7 +23,7 @@
 
 <div class="columns-2 px-2 sm:columns-3 sm:px-4 md:px-6 lg:px-20 lg:columns-4 py-2 gap-x-2">
     {#each notes as note}
-        <div class=" duration-200 bg-neutral-900 hover:shadow-pink-500 border-2 my-1 sm:my-1 text-[12px] sm:text-[14px] px-2 pt-2 sm:pt-3 pb-4 sm:pb-10 hover:bg-neutral-800 cursor-pointer rounded-lg border-pink-500 inline-block w-full">
+        <div class=" duration-200 bg-neutral-900 hover:shadow-pink-500 border-2 my-1 sm:my-1 text-[11px] sm:text-[14px] px-2 pt-2 sm:pt-3 pb-4 sm:pb-10 hover:bg-neutral-800 cursor-pointer rounded-lg border-pink-500 inline-block w-full">
             {note}
         </div>
     {/each}
