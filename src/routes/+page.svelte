eec025df29d7af70946b1004a9de803b851d6191
apooravm <medal.apoorav@gmail.com>
2024-02-24 22:22:42 +0530

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import Scene from "./threlte/Scene.svelte";
+    import { Canvas } from "@threlte/core";
+
     const siteRoutes = [
         {
             title: "notes",
@@ -38,22 +41,27 @@
     ]
 </script>
 
-<div class="flex flex-row w-full h-full font-yanone main-cont">
-	<div class="flex flex-col p-8 select-none justify-between w-full">
+<div class="relative flex flex-row w-full h-full font-yanone">
+    <div class="absolute w-full h-full">
+        <Canvas>
+            <Scene />
+        </Canvas>
+    </div>
+	<div class="z-50 flex flex-col sm:p-8 p-4 select-none justify-between w-full">
 		<div class="flex flex-row justify-between w-full">
             <div class="flex flex-col">
-                <span class="font-thin text-6xl">Apoorav Medal</span>
-                <span class="font-thin">Developer & learner</span>
+                <span class="font-thin sm:text-6xl text-2xl"><span class="font-semibold">Apoorav</span> Medal</span>
+                <span class="font-thin sm:text-base text-xs">Developer & learner</span>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col sm:text-base text-sm">
                 <span>contact</span>
                 {#each contactLinks as link}
                     <a class="font-thin underline" href={link.link}>{link.title}</a>
                 {/each}
             </div>
         </div>
-        <div class="flex flex-col">
-            <span>stuff</span>
+        <div class="flex flex-col sm:text-base text-sm">
+            <span class="font-semibold">stuff</span>
             {#each siteRoutes as link}
                 <a class="font-thin underline" href={link.link}>{link.title}</a>
             {/each}
